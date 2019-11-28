b8a63ec44e36871d47b125c8abf55779533d4ba7
gkopplin <grant.kopplin@gmail.com>
2019-11-28 11:50:50 -0500

    Add spring boot test annotation

--- src/test/java/com/example/postsapi/service/PostServiceTest.java
+++ src/test/java/com/example/postsapi/service/PostServiceTest.java
@@ -9,18 +9,19 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.mockito.ArgumentMatchers.any;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 @RunWith(MockitoJUnitRunner.class)
+@SpringBootTest
 public class PostServiceTest {
 
     @InjectMocks
